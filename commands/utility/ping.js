735beec4005f81de04de052624419064ba85f5b2
Euwee <eusola3@addu.edu.ph>
2024-12-18 15:58:44 +0800

    initial commit

---
+++ commands/utility/ping.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    cooldown: 5,
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
+    async execute(interaction) {
+        // Check if the interaction has already been replied to
+        if (interaction.deferred || interaction.replied) {
+            await interaction.editReply('Pong!');
+        } else {
+            await interaction.reply('Pong!');
+        }
+    },
+};
